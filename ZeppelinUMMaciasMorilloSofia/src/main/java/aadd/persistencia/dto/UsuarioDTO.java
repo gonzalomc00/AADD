c7e613de25d7c791872a2e2c82feb527c5cc2855
Gonzalo <gonzalo.manzanaresc@um.es>
2022-12-05 19:57:18 +0100

    Boletín JSF I

--- ZeppelinUMMaciasMorilloSofia/src/main/java/aadd/persistencia/dto/UsuarioDTO.java
+++ ZeppelinUMMaciasMorilloSofia/src/main/java/aadd/persistencia/dto/UsuarioDTO.java
@@ -17,13 +17,15 @@ public class UsuarioDTO implements Serializable{
 	protected String apellidos;
 	protected TipoUsuario tipo;
 	protected LocalDate fechaNacimiento;
+	protected String email;
 
-	public UsuarioDTO(Integer id, String nombre, String apellidos, LocalDate fechaNacimiento, TipoUsuario tipo) {
+	public UsuarioDTO(Integer id, String nombre, String apellidos, LocalDate fechaNacimiento, TipoUsuario tipo, String email) {
 		this.id = id;
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 		this.fechaNacimiento = fechaNacimiento;
 		this.tipo = tipo;
+		this.email=email;
 	}
 	// getters y setters
 
@@ -65,6 +67,14 @@ public class UsuarioDTO implements Serializable{
 
 	public void setFechaNacimiento(LocalDate fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 	
