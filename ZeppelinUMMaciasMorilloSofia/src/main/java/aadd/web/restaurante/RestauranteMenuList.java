de4586fdc266f97fba487b7407245c6ed530c67a
Gonzalo <gonzalo.manzanaresc@um.es>
2022-12-08 19:27:10 +0100

    Logica del pedido

--- ZeppelinUMMaciasMorilloSofia/src/main/java/aadd/web/restaurante/RestauranteMenuList.java
+++ ZeppelinUMMaciasMorilloSofia/src/main/java/aadd/web/restaurante/RestauranteMenuList.java
@@ -1,6 +1,7 @@
 package aadd.web.restaurante;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -40,9 +41,15 @@ public class RestauranteMenuList implements Serializable {
 
 	private Integer userId;
 	private boolean responsable;
+	
+	//PEDIDO
+	private HashMap<Integer,Integer> pedido;
+	private Integer cantidad;
 
 	public RestauranteMenuList() {
 		servicio = ServicioGestionPlataforma.getServicioGestionPlataforma();
+		cantidad=0;
+		pedido= new HashMap<Integer,Integer>();
 	}
 
 	@PostConstruct
@@ -150,6 +157,44 @@ public class RestauranteMenuList implements Serializable {
 
 	public void setResponsable(boolean responsable) {
 		this.responsable = responsable;
+	}
+
+	public HashMap<Integer,Integer> getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(HashMap<Integer,Integer> pedido) {
+		this.pedido = pedido;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public void sumarCantidad(Integer plato) {
+		Integer cantidad=pedido.get(plato);
+		if(cantidad==null) {
+			pedido.put(plato, 1);
+		}
+		else {
+			pedido.put(plato, cantidad++);
+		}
+	}
+	
+	public void restarCantidad(Integer plato) {
+		Integer cantidad=pedido.get(plato);
+		if(cantidad!=null) {
+			if(cantidad--==0) {
+				pedido.remove(plato);
+			} else {
+			pedido.put(plato, cantidad--);
+			}
+		}
+	
 	}
 
 }
