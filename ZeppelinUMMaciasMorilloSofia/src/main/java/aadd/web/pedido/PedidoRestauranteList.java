cc8fa5f277e8183902bfb763d0779f6b7b907e8e
Gonzalo <gonzalo.manzanaresc@um.es>
2022-12-28 00:45:34 +0100

    Añadido la restricción de acceso - falta la página de pedidos del restaurante

--- ZeppelinUMMaciasMorilloSofia/src/main/java/aadd/web/pedido/PedidoRestauranteList.java
+++ ZeppelinUMMaciasMorilloSofia/src/main/java/aadd/web/pedido/PedidoRestauranteList.java
@@ -23,68 +23,71 @@ public class PedidoRestauranteList implements Serializable {
 
 	@Inject
 	private FacesContext facesContext;
-	
+
 	private List<PedidoDTO> pedidos;
 	private List<IncidenciaDTO> incidencias;
 	private Integer restauranteId;
 	private ServicioGestionPedido servicio;
 
 	private ServicioGestionPlataforma servicioPlataforma;
-	
+
 	private String comentarioIncidencia;
 	private Integer incidenciaSeleccionada;
-	
-	
-	public PedidoRestauranteList() {
-		servicio= ServicioGestionPedido.getServicioGestionPedido();
-		servicioPlataforma= ServicioGestionPlataforma.getServicioGestionPlataforma();
-	}
 
+
+
+	public PedidoRestauranteList() {
+		servicio = ServicioGestionPedido.getServicioGestionPedido();
+		servicioPlataforma = ServicioGestionPlataforma.getServicioGestionPlataforma();
+	}
 	
+
+
 	public void loadPedidos() {
-		pedidos=servicio.findPedidoByRestaurante(restauranteId);
-		
+
+		pedidos = servicio.findPedidoByRestaurante(restauranteId);
+
 	}
 
 	public void loadIncidencias() {
-		incidencias=servicioPlataforma.getIncidenciasByRestauranteSinCerrar(restauranteId);
+		incidencias = servicioPlataforma.getIncidenciasByRestauranteSinCerrar(restauranteId);
 	}
-	
+
 	public void seleccionarIncidencia(Integer id_incidencia) {
-		incidenciaSeleccionada=id_incidencia;
+		incidenciaSeleccionada = id_incidencia;
 	}
-	
-	public void cerrarIncidencia()
-	{
-		if(servicioPlataforma.cerrarIncidencia(incidenciaSeleccionada, comentarioIncidencia)) {
-			 facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "La incidencia se ha cerrado correctatmente", ""));
-			 loadIncidencias();
+
+	public void cerrarIncidencia() {
+		if (servicioPlataforma.cerrarIncidencia(incidenciaSeleccionada, comentarioIncidencia)) {
+			facesContext.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "La incidencia se ha cerrado correctamente", ""));
+			loadIncidencias();
 		} else {
-			  facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "La incidencia no se ha cerrado correctamente", ""));
+			facesContext.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "La incidencia no se ha cerrado correctamente", ""));
 
 		}
 	}
+
 	public void cambiarEstadoPedido(ObjectId id, String estado) {
-		switch(estado) {
-			case "ACEPTADO":
-				servicio.editarEstado(id, TipoEstado.ACEPTADO);
-				break;
-			case "CANCELADO":
-				servicio.editarEstado(id, TipoEstado.CANCELADO);
-				break;
-			case "PREPARADO":
-				servicio.editarEstado(id, TipoEstado.PREPARADO);
-				break;
-			case "RECOGIDO":
-				servicio.editarEstado(id, TipoEstado.RECOGIDO);
-				break;
-			
-			
+		switch (estado) {
+		case "ACEPTADO":
+			servicio.editarEstado(id, TipoEstado.ACEPTADO);
+			break;
+		case "CANCELADO":
+			servicio.editarEstado(id, TipoEstado.CANCELADO);
+			break;
+		case "PREPARADO":
+			servicio.editarEstado(id, TipoEstado.PREPARADO);
+			break;
+		case "RECOGIDO":
+			servicio.editarEstado(id, TipoEstado.RECOGIDO);
+			break;
+
 		}
 		loadPedidos();
 	}
-	
-	
+
 	public FacesContext getFacesContext() {
 		return facesContext;
 	}
@@ -109,57 +112,45 @@ public class PedidoRestauranteList implements Serializable {
 		this.restauranteId = restauranteId;
 	}
 
-
 	public ServicioGestionPedido getServicio() {
 		return servicio;
 	}
-
 
 	public void setServicio(ServicioGestionPedido servicio) {
 		this.servicio = servicio;
 	}
 
-
 	public List<IncidenciaDTO> getIncidencias() {
 		return incidencias;
 	}
-
 
 	public void setIncidencias(List<IncidenciaDTO> incidencias) {
 		this.incidencias = incidencias;
 	}
 
-
 	public ServicioGestionPlataforma getServicioPlataforma() {
 		return servicioPlataforma;
 	}
-
 
 	public void setServicioPlataforma(ServicioGestionPlataforma servicioPlataforma) {
 		this.servicioPlataforma = servicioPlataforma;
 	}
 
-
 	public String getComentarioIncidencia() {
 		return comentarioIncidencia;
 	}
-
 
 	public void setComentarioIncidencia(String comentarioIncidencia) {
 		this.comentarioIncidencia = comentarioIncidencia;
 	}
 
-
 	public Integer getIncidenciaSeleccionada() {
 		return incidenciaSeleccionada;
 	}
 
-
 	public void setIncidenciaSeleccionada(Integer incidenciaSeleccionada) {
 		this.incidenciaSeleccionada = incidenciaSeleccionada;
 	}
-	
-	
-	
-	
+
+
 }
